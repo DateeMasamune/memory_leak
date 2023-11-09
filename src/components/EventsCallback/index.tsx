628adc2bcdca268d130e18572323106431c55301
Dmitry <ovsienkodima2@gmail.com>
2023-11-09 17:12:37 +0300

    new leak memory

--- src/components/EventsCallback/index.tsx
+++ src/components/EventsCallback/index.tsx
@@ -1,29 +1,45 @@
 import { Layout } from "../Layout";
-import { useCallback, useEffect, useState } from "react";
-import styles from './styles.module.scss'
+import { useEffect, useRef, useState } from "react";
+import styles from "./styles.module.scss";
+
+/**
+ * 
+ * Включаю запись бегаю мышкой по экрану без удаления события
+ * переключаюсь на пустую страницу бегаю там потом жду и выключаю запись (утечка)
+ * 
+ * Включаю запись бегаю мышкой по экрану с удалением события
+ * переключаюсь на пустую страницу бегаю там потом жду и выключаю запись (устранение утечки)
+ */
 
 export const EventsCallback = () => {
-  const [coords, setCoords] = useState({ y: 0, x: 0 })
+  const [coords, setCoords] = useState({ y: 0, x: 0 });
+  const saveCoords = useRef<{ y: number; x: number }[]>([]);
 
-  const handlerCoords = useCallback((event: MouseEvent) => {
-    setCoords({
+  const doSomething = <T,>(gigantObject: T) => gigantObject;
+
+  const handlerCoords = (event: MouseEvent) => {
+    const coord = {
       y: event.clientY,
-      x: event.clientX
-    })
-  }, [])
+      x: event.clientX,
+    };
+    setCoords(coord);
+    saveCoords.current.push(coord);
+    console.log("==========>saveCoords", saveCoords);
+    doSomething(saveCoords);
+  };
 
   useEffect(() => {
-    window.addEventListener('mousemove', handlerCoords)
+    window.addEventListener("mousemove", handlerCoords);
 
     return () => {
-      window.removeEventListener('mousemove', handlerCoords)
-    }
-  }, [handlerCoords])
+      window.removeEventListener("mousemove", handlerCoords);
+    };
+  }, []);
 
   return (
     <Layout className={styles.center}>
       <div>Координаты Y: {coords.y}</div>
       <div>Координаты X: {coords.x}</div>
     </Layout>
-  )
+  );
 };
--- src/components/Timers/index.tsx
+++ src/components/Timers/index.tsx
@@ -1,23 +1,40 @@
-import { useEffect, useState } from "react";
-import styles from './styles.module.scss'
+import { useEffect, useRef, useState } from "react";
+import styles from "./styles.module.scss";
 import { Layout } from "../Layout";
 
+/**
+ *
+ * Запускаем таймер без отключения и включаем запись
+ * переходим на пустую страницу ждем и останавливаем запись (утечка)
+ *
+ * Запускаем таймер с отключением и включаем запись
+ * переходим на пустую страницу и останавливаем запись (решение проблемы с утечкой)
+ */
+
 export const Timers = () => {
-  const time = new Date().toLocaleTimeString()
-  const [ctime, setTime] = useState(time)
+  const time = new Date().toLocaleTimeString();
+  const [ctime, setCtime] = useState(time);
+  const saveTime = useRef<{ times: string[] }>({ times: [] });
 
   useEffect(() => {
-    setInterval(() => {
-      const time = new Date().toLocaleTimeString()
-      setTime(time)
-    })
-  }, [])
+    const interval = setInterval(() => {
+      const time = new Date().toLocaleTimeString();
+      setCtime(time);
+      saveTime.current.times.push(`${time} `);
+      console.log('==========>saveTime.', saveTime.current.times);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <Layout className={styles.center}>
       <div className={styles.wrapper}>
         <div className={styles.clock}>{ctime}</div>
+        <div>{saveTime.current.times}</div>
       </div>
     </Layout>
-  )
+  );
 };
